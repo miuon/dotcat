4fe70fcfa1a50767bc245ddf6b40d75620d87473
Andrew S <miuon@protonmail.com>
2020-06-10 09:21:05 -0400

    implement fake theme build (just copy files)

--- modot.py
+++ modot.py
@@ -4,6 +4,7 @@ import asyncio
 import chevron
 import click
 import os
+import shutil
 import sys
 import yaml
 
@@ -70,25 +71,36 @@ def link_color(color_opt, color_dir, default_color):
     return (color_found, color_changed)
 
 def build_templates(modules):
+    built_theme_dir = CONFIG_DIR / BUILT_THEME
+    # TODO: make atomic?
+    shutil.rmtree(built_theme_dir)
+    built_theme_dir.mkdir()
     with open(CONFIG_DIR / COLOR, 'r') as stream:
         color_dict = yaml.safe_load(stream)
     for module in modules:
-        theme_module_dir = CONFIG_DIR / THEME / Path(module.subdir)
-        built_theme_module_dir = CONFIG_DIR / BUILT_THEME / Path(module.subdir)
+        subdir = module.get('subdir', '')
+        if not subdir:
+            continue # maybe log or something
+        theme_module_dir = CONFIG_DIR / THEME / Path(subdir)
+        built_theme_module_dir = built_theme_dir / Path(subdir)
         module_theme_files = [item for item in
                 theme_module_dir.rglob('*') if item.is_file()]
         for theme_file in module_theme_files:
-            base_file_path = theme_module_dir / theme_file
-            gen_file_path = built_theme_module_dir / theme_file
+            base_file_path = theme_file
+            gen_file_path = (
+                built_theme_module_dir / theme_file.relative_to(theme_module_dir))
             template(color_dict, base_file_path, gen_file_path)
 
 def link(tgt, link_name):
     """ Forcibly create symlink from link_name to src. """
-    os.symlink(tgt, str(link_name) + '.modottmp')
-    os.rename(str(link_name) + '.modottmp', link_name)
+    tmp_path = link_name.parent / Path('modottmp')
+    os.symlink(tgt, tmp_path)
+    os.rename(tmp_path, link_name)
 
 def template(color_dict, src_path, tgt_path):
     """ Template the src file against the colors to create the tgt file. """
+    tgt_path.parent.mkdir(parents=True, exist_ok=True)
+    shutil.copy(src_path, tgt_path)
 
 async def run_script(script):
     cmd = ' '.join((str(script),
