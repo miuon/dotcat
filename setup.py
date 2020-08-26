16338176543a3306052d9a38308db6f97d5677f5
miuon <andrew@miuon.net>
2020-08-25 22:44:51 -0400

    setuptools but nicer

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
 '''Package configuraton for modot (MODular DOtfiles)'''
-from setuptools import setup
-setup(
+import setuptools
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
     name='modot',
     version='0.5.1',
     description='MOdular DOTfiles',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/miuon/modot',
     author='miuon',
     author_email='andrew@miuon.net',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+
     packages=['modot'],
     test_suite='tests',
-
     python_requires=">=3.7",
     install_requires=[
         'chevron',
